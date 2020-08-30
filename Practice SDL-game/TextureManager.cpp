1287f15c66a06804c91f5685f31142fd26f722f6
Anastasiia Sotnikova <nastiksot02@gmail.com>
2020-08-31 01:03:07 +0300

    create load funcion in TM

--- Practice SDL-game/TextureManager.cpp
+++ Practice SDL-game/TextureManager.cpp
@@ -1,8 +1,17 @@
 #include "TextureManager.h"
 
-bool TextureManager::load(string filename, string id, SDL_Renderer* pRenderer)
+bool TextureManager::load(string fileName, string id, SDL_Renderer* pRenderer)
 {
-    return false;
+    //load the image to the surface
+    SDL_Surface* pTempSurface = IMG_Load(fileName.c_str());
+    if (pTempSurface == 0) 
+    {
+        cout << "initialization surface failed" << endl;
+        return false;
+    }
+    //create texture from surface n image
+    SDL_Texture* pTexture = SDL_CreateTextureFromSurface(pRenderer,pTempSurface);
+    SDL_FreeSurface(pTempSurface);
 }
 
 void TextureManager::draw(string id, int x, int y, int width, int height, SDL_Renderer* pRenderer, SDL_RendererFlip)
